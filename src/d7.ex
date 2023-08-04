e07eba5f78b4f8bfb9d981b1051a33a1865457d0
Karan Godara <karan@getquilt.app>
2023-08-04 03:05:39 -0400

    refactored/streamlined day 7 with structs

--- src/d7.ex
+++ src/d7.ex
@@ -1,67 +1,40 @@
 
+defmodule Entry do
+  defstruct name: "", type: nil, size: 0, children: []
+end
 
 # NOTE: directory names are not unique, assume files names are not either
 defmodule SpaceManager do
 
 
-  def parse_layout([], _, file_size_lookup, children_lookup, type_lookup) do
-    {file_size_lookup, children_lookup, type_lookup}
+  def parse_layout([], _, entry_lookup) do
+    entry_lookup
   end
 
-
-  # path: ["b", "a", "/"]
-  # NOTE: can use an Entry struct or something to avoid both size_lookup & children_lookup
-  def parse_layout(cmd_list, path, file_size_lookup, children_lookup, type_lookup) when length(cmd_list) > 0 do
-    # [0-9]: add file_name:size keypair to map
-    # dir: 
-
-    # on: '$ cd $DIR_NAME' --> modify path and continue
-    # on: '$ cd ..' --> modify path and continue
-    # on: '$ ls' --> add all FILES to map
-    # on: '4358974 $FILE.NAME' --> 
-    #     add (file_name => file_size) -- file_size_lookup
-    #     add (cwd_path => [file_name] ++ val) -- children_lookup
-    #     add (file_name => :file) -- type_lookup
-    # on: ''
-
+  def parse_layout(cmd_list, path, entry_lookup) when length(cmd_list) > 0 do
+    
     {cur_cmd, n_cmd_list} = List.pop_at(cmd_list, 0)
     cwd_path = Enum.join(Enum.reverse(path), "/")
 
-    [n_path, n_file_size_lookup, n_children_lookup, n_type_lookup] = cond do
-      # "$ cd .."
+    # handle cd commands
+    n_path = cond do
       cur_cmd == "$ cd .." ->
-
-        new_path = Enum.drop(path, 1)
-        [
-          new_path,
-          file_size_lookup,
-          children_lookup,
-          type_lookup
-        ]
+        Enum.drop(path, 1)
 
       # "$ cd $DIR_NAME"
       String.match?(cur_cmd, ~r/\$ cd .*/) ->
+        cur_cmd |>
+        String.split(" ") |>
+        Enum.at(2) |>
+        List.wrap |>
+        Kernel.++(path)
 
-        new_cwd = String.split(cur_cmd, " ") |>
-          Enum.at(2)
+      true ->
+        path
+    end
 
-        new_path = [new_cwd] ++ path
-
-        [
-          new_path,
-          file_size_lookup,
-          children_lookup,
-          type_lookup
-        ]
-
-      # "$ ls"
-      cur_cmd == "$ ls" ->
-        [
-          path,
-          file_size_lookup,
-          children_lookup,
-          type_lookup
-        ]
+    # handle file/dir entries
+    n_entry_lookup = cond do
 
       # "4358974 $FILE.NAME"
       String.match?(cur_cmd, ~r/[0-9]+ .*/) ->
@@ -69,16 +42,17 @@ defmodule SpaceManager do
         [file_size, file_name] = String.split(cur_cmd, " ")
         full_file_name = cwd_path <> "/" <> file_name
 
-        new_file_size_lookup = Map.put(file_size_lookup, full_file_name, String.to_integer(file_size))
-        new_children_lookup = Map.get_and_update(children_lookup, cwd_path, fn val -> {val, [full_file_name] ++ val} end) |> elem(1)
-        new_type_lookup = Map.put(type_lookup, full_file_name, :file)
-
-        [
-          path,
-          new_file_size_lookup,
-          new_children_lookup,
-          new_type_lookup
-        ]
+        Map.put(
+          entry_lookup,
+          full_file_name,
+          %Entry{
+            name: full_file_name,
+            type: :file,
+            size: String.to_integer(file_size),
+            children: []
+          }
+        ) |>
+        Map.update!(cwd_path, fn entry -> %{entry | children: [full_file_name] ++ entry.children} end)
 
       # "dir $DIR_NAME"
       String.match?(cur_cmd, ~r/dir .*/) ->
@@ -86,111 +60,99 @@ defmodule SpaceManager do
         [_, dir_name] = String.split(cur_cmd, " ")
         full_dir_name = cwd_path <> "/" <> dir_name
 
-        # add dir to new_children_lookup as key and val (parent & child)
-        new_children_lookup = Map.put(children_lookup, full_dir_name, [])
-        new_children_lookup = Map.get_and_update(new_children_lookup, cwd_path, fn val -> {val, [full_dir_name] ++ val} end) |> elem(1)
-        new_type_lookup = Map.put(type_lookup, full_dir_name, :dir)
+        Map.put(
+          entry_lookup,
+          full_dir_name,
+          %Entry{
+            name: full_dir_name,
+            type: :dir,
+          }
+        ) |>
+        Map.update!(cwd_path, fn entry -> %{entry | children: [full_dir_name] ++ entry.children} end)
 
-        [
-          path,
-          file_size_lookup,
-          new_children_lookup,
-          new_type_lookup
-        ]
+      true ->
+        entry_lookup
     end
 
-    SpaceManager.parse_layout( n_cmd_list, n_path, n_file_size_lookup, n_children_lookup, n_type_lookup)
+    SpaceManager.parse_layout( n_cmd_list, n_path, n_entry_lookup )
   end
 
 
-  def fill_dir_sizes([], dir_size_lookup, file_size_lookup, _, _) do
-    Map.merge(file_size_lookup, dir_size_lookup)
+  def fill_dir_sizes([], entry_lookup) do
+    entry_lookup
   end
 
 
-  def fill_dir_sizes([{name, children} | tail], dir_size_lookup, file_size_lookup, children_lookup, type_lookup) do
+  def fill_dir_sizes([dir_entry | tail], entry_lookup) do
 
-    dir_size = Enum.reduce(children, 0, fn entry_name, acc_size ->
-      entry_type = Map.get(type_lookup, entry_name)
-
-      size_val = (if entry_type == :dir, do: dir_size_lookup, else: file_size_lookup) |>
-        Map.get(entry_name)
-
-      acc_size + size_val
-    end)
+    dir_size = dir_entry.children |>
+      Enum.map(& Map.get(entry_lookup, &1).size ) |>
+      Enum.reduce(& &1 + &2)
 
     SpaceManager.fill_dir_sizes(
       tail,
-      Map.put(dir_size_lookup, name, dir_size),
-      file_size_lookup,
-      children_lookup,
-      type_lookup
+      Map.update!(entry_lookup, dir_entry.name, fn entry -> %{entry | size: dir_size} end)
     )
   end
 
 
-  def fill_dir_sizes(file_size_lookup, children_lookup, type_lookup) do
+  def fill_dir_sizes(entry_lookup) do
     # sort by descending dir depth,
     # we want to resolve the tree bottom-up
 
-    dir_size_by_depth = type_lookup |>
-      Enum.filter(fn {_, type} -> type == :dir end) |>
-      Enum.map(fn {name, _} ->
-        { name,
-          String.graphemes(name) |> Enum.count(& &1 == "/"),
+    dir_by_depth = entry_lookup |>
+      Map.values |>
+      Enum.filter(& &1.type == :dir) |>
+      Enum.map(fn e ->
+        { e,
+          String.graphemes(e.name) |> Enum.count(& &1 == "/"),
         }
       end) |>
       Enum.sort(fn {_, depth1}, {_, depth2} -> depth1 >= depth2 end) |>
-      Enum.map(fn {name, _depth} -> 
-        {
-          name,
-          Map.get(children_lookup, name)
-        }
-      end)
+      Enum.map(& elem(&1, 0))
 
-    SpaceManager.fill_dir_sizes(dir_size_by_depth, Map.new(dir_size_by_depth), file_size_lookup, children_lookup, type_lookup) |> Map.new
+    SpaceManager.fill_dir_sizes(dir_by_depth, entry_lookup)
+
   end
 
-
-  def find_to_delete(full_size_lookup, type_lookup, total_space, needed_free_space) do
+  def find_to_delete(entry_lookup, total_space, needed_free_space) do
   
-    used_space = full_size_lookup |> Map.get("/")
+    used_space = entry_lookup |> Map.get("/")
+    used_space = used_space.size
 
     available_space = total_space - used_space
     space_to_make = needed_free_space - available_space
 
-    type_lookup |>
-    Enum.filter(fn {_, type} -> type == :dir end) |>
-    Enum.map(fn {name, _type} -> Map.get(full_size_lookup, name) end) |>
-    Enum.filter(& &1 >= space_to_make) |>
-    Enum.reduce(& min &1, &2)
+    entry_lookup |>
+    Map.values |>
+    Enum.filter(& &1.type == :dir) |>
+    Enum.filter(& &1.size >= space_to_make) |>
+    Enum.reduce(700000001, & min(&1.size, &2))
+
   end
 end
 
 
-lines = File.read!("../input/d7.txt") |> String.split("\n")
+defmodule Main do
+  def main() do
+    lines = File.read!("../input/d7.txt") |> String.split("\n")
 
-{file_size_lookup, children_lookup, type_lookup} = SpaceManager.parse_layout(lines, [], %{}, %{"/" => []}, %{"/" => :dir})
+    entry_lookup = SpaceManager.parse_layout(lines, [], %{"/" => %Entry{name: "/", type: :dir, size: 0, children: []}})
+    entry_lookup = SpaceManager.fill_dir_sizes(entry_lookup)
 
+    p1_size = 
+        entry_lookup |>
+        Map.values |>
+        Enum.filter(& &1.type == :dir) |>
+        Enum.filter(& &1.size <= 100000) |>
+        Enum.reduce(0, & &1.size + &2)
 
-full_size_lookup = SpaceManager.fill_dir_sizes(file_size_lookup, children_lookup, type_lookup)
+    p2_to_delete = SpaceManager.find_to_delete(entry_lookup, 70000000, 30000000)
 
-p1_size = 
-    type_lookup |>
-    Enum.filter(fn {_, type} -> type == :dir end) |>
-    Enum.map(fn {name, _type} ->
-      Map.get(full_size_lookup, name)
-    end) |>
-    Enum.filter(& &1 <= 100000) |>
-    Enum.reduce(& &1 + &2)
+    IO.puts("Part 1: #{p1_size}")
+    IO.puts("Part 2: #{p2_to_delete}")
+  end
+end
 
-p2_to_delete = SpaceManager.find_to_delete(full_size_lookup, type_lookup, 70000000, 30000000)
-
-IO.puts("Part 1: #{p1_size}")
-IO.puts("Part 2: #{p2_to_delete}")
-
-
-
-
-
+Main.main
 
