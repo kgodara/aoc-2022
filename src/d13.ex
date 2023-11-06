da898afeea23e4d31ff5c9b8e48f7c46d8b4f49b
Karan Godara <karan@getquilt.app>
2023-11-06 08:55:16 -0500

    day 13 minor refactor / cleaning

--- src/d13.ex
+++ src/d13.ex
@@ -14,8 +14,8 @@ defmodule DistressSignal do
       leading_token_trimmed = binary_slice(data, 1..byte_size(data))
 
       n_depth = depth + cond do
-        String.match?(data, ~r/^\[.*/) -> 1
-        String.match?(data, ~r/^\].*/) -> -1
+        data =~ ~r/^\[.*/ -> 1
+        data =~ ~r/^\].*/ -> -1
         true -> 0
       end
 
@@ -40,7 +40,7 @@ defmodule DistressSignal do
       # {type, elem, remaining}
       cond do
         # start of list 
-        String.match?(data, ~r/^\[.*/) ->
+        data =~ ~r/^\[.*/ ->
 
           {end_, rem} = DistressSignal.find_list_rbound(binary_slice(data, 1..byte_size(data)), 1, 0)
 
@@ -51,7 +51,7 @@ defmodule DistressSignal do
           {:list, binary_part(data, 1, end_-1), rem}
 
         # match leading any-num-digit integer
-        String.match?(data, ~r/^[0-9]+/) ->
+        data =~ ~r/^[0-9]+/ ->
           leading_int = Regex.run(~r/^[0-9]+/, data, capture: :first) |> List.first
           {:int, leading_int, binary_slice(data, byte_size(leading_int)..byte_size(data))}
       end
@@ -64,27 +64,20 @@ defmodule DistressSignal do
 
       case {l_type, r_type} do
         {:int, :int} ->
-          [l_int, r_int] = [l_val, r_val] |> Enum.map(& String.to_integer(&1))
+          [l_int, r_int] = [l_val, r_val] |> Enum.map(& String.to_integer/1)
           cond do
             l_int > r_int -> false
             l_int < r_int -> true
             l_int == r_int -> DistressSignal.compare(l_rem, r_rem)
           end
-        {:list, :int} ->
-          DistressSignal.compare(l_val, r_val)
-        {:int, :list} ->
-          DistressSignal.compare(l_val, r_val)
-        {:list, :list} ->
+        {:list, :list} when l_val == "" and r_val == "" ->
           # case where two empty lists co-occur,
           # and parsing should skip trying to evaluate list contents
-          if byte_size(l_val) == 0 && byte_size(r_val) == 0 do
-            DistressSignal.compare(l_rem, r_rem)
-          else
-            DistressSignal.compare(l_val, r_val)
-          end
+          DistressSignal.compare(l_rem, r_rem)
+        {l, r} when l in [:list, :int] and r in [:list, :int] ->
+          DistressSignal.compare(l_val, r_val)
         {l, nil} when l != nil -> false
-        {nil, r} when r != nil -> true
-        {nil, nil} -> true
+        {nil, _} -> true
       end
     end
 end
@@ -92,8 +85,7 @@ end
 
 lines = File.read!("../input/d13.txt") |>
   String.trim_trailing |>
-  String.split("\n") |>
-  Enum.filter(fn line -> String.length(line) > 0 end)
+  String.split("\n", trim: true)
 
 # Part 1 sol
 res = lines |>
