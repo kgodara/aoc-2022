a65154edb835f1a37ae1006837155edc596cb210
Karan Godara <karan@getquilt.app>
2024-03-14 12:23:44 -0400

    d24 p1 & p2 sol - fixed perf

--- src/d24.ex
+++ src/d24.ex
@@ -1,109 +1,144 @@
 
+defmodule Board do
+  # Note: Problem with the matrix approach is that things don't move in same dir ->
+  # relative distances between cells will change ->
+  # need to separate to two groups: {right, down}, {left, up}
+  defstruct [:d_r_mat, :u_l_mat, :right_bound, :down_bound]
 
+  def from_lines(lines) do
 
-defmodule BlizzardBasin do
-
-  def parse(lines) do
-    # drop up/down walls
-    lines =
+    rows_all =
       lines |>
-      Enum.slice(1..length(lines)-2)
+      # drop up/down walls
+      Enum.slice(1..length(lines)-2) |>
+      Enum.map(& String.graphemes/1) |>
+      # drop left/right walls
+      Enum.map(& Enum.slice(&1, 1..length(&1)-2)) |>
+      Enum.reduce([], fn row, matrix ->
 
-    lines |>
-    Enum.map(& String.graphemes/1) |>
-    # drop left/right walls
-    Enum.map(& Enum.slice(&1, 1..length(&1)-2)) |>
-    Enum.with_index |>
-    Enum.reduce(%{}, fn {row, row_idx}, blizz_acc ->
-      row |>
-      Enum.with_index |>
-      Enum.reduce(blizz_acc, fn {ch, col_idx}, blizz_row_acc ->
-        case ch do
-          "." -> blizz_row_acc
-          blizz ->
-            dir = case blizz do
+        n_row =
+          row |>
+          Enum.map(fn ch ->
+            case ch do
+              "." -> :empty
               "<" -> :left
               "^" -> :up
               ">" -> :right
               "v" -> :down
             end
-
-            Map.put(blizz_row_acc, {row_idx, col_idx}, [dir])
-        end
-      end)
-    end)
-  end
-
-  def bounds_and_dst(lines) do
-    # last idx that is valid
-    down_bound_idx = (lines |> length) - 2 - 1
-    right_bound_idx = (lines |> hd |> String.graphemes |> length) - 2 - 1
-
-    dst = {down_bound_idx, right_bound_idx}
-
-    {down_bound_idx, right_bound_idx, dst}
-  end
-
-  # Memoize by time-step
-  def advance_blizzards(blizz_lookup, steps_taken, {down_bound, right_bound}) do
-
-    case Map.has_key?(blizz_lookup, steps_taken) do
-      true -> blizz_lookup
-      false ->
-        prev_state = Map.get(blizz_lookup, steps_taken-1)
-
-        n_state =
-          prev_state |>
-          Enum.map(fn {{r, c}, dir_list} ->
-            dir_list |>
-            Enum.map(fn dir ->
-              {n_r, n_c} = case dir do
-                :left -> {r, c-1}
-                :up -> {r-1, c}
-                :right -> {r, c+1}
-                :down -> {r+1, c}
-              end
-
-              # Update if need to wrap
-              {n_r, n_c} = cond do
-                n_r < 0 -> {down_bound, n_c}
-                n_c < 0 -> {n_r, right_bound}
-                n_r > down_bound -> {0, n_c}
-                n_c > right_bound -> {n_r, 0}
-                true -> {n_r, n_c}
-              end
-
-              {{n_r, n_c}, dir}
-            end)
-          end) |>
-          List.flatten |>
-          # Need to update dir lists for each cell,
-          # e.g. create list or
-          Enum.reduce(%{}, fn {{r,c},dir}, blizz_acc ->
-            blizz_acc |>
-            Map.put_new_lazy({r,c}, fn -> [] end) |>
-            Map.update!({r,c}, & [dir] ++ &1)
           end)
 
-        Map.put(blizz_lookup, steps_taken, n_state)
-    end
+        matrix ++ [n_row]
+      end)
+
+    rows_d_r =
+      rows_all |>
+      Enum.map(fn row ->
+        Enum.map(row, fn cell ->
+          case cell do
+            x when x in [:down, :right] -> x
+            _ -> :empty
+          end
+        end)
+      end)
+
+
+    rows_u_l =
+      rows_all |>
+      Enum.map(fn row ->
+        Enum.map(row, fn cell ->
+          case cell do
+            x when x in [:left, :up] -> x
+            _ -> :empty
+          end
+        end)
+      end)
+
+    %Board{ d_r_mat: rows_d_r, u_l_mat: rows_u_l } |>
+    Board.bounds
   end
+
+  def bounds(%Board{} = board) do
+    # last idx that is valid
+    down_bound_idx = (board.d_r_mat |> length) - 1
+    right_bound_idx = (board.d_r_mat |> hd |> length) - 1
+
+    %Board{ board | right_bound: right_bound_idx, down_bound: down_bound_idx}
+  end
+
+  def adj_empty_at_tick(board, {r, c}, start, ticks_to_simulate) do
+
+    adj_list = [
+      {r, c+1},
+      {r+1, c},
+      {r,c},
+      {r, c-1},
+      {r-1, c},
+    ]
+
+    adj_list |>
+    Enum.filter(fn {n_r, n_c} ->
+      # allow option to remain at start pos if we haven't left it yet
+      ({n_r, n_c} == start and {r,c} == start) or (n_r >= 0 and n_c >= 0 and n_r <= board.down_bound and n_c <= board.right_bound)
+    end) |>
+    Enum.filter(fn {n_r, n_c} ->
+
+      num_cols = board.right_bound + 1
+      num_rows = board.down_bound + 1
+
+      # ticks_to_simulate % row_len
+      shift_horiz = rem(ticks_to_simulate, num_cols)
+      right_cell = rem(n_c - shift_horiz + num_cols, num_cols)
+      # add num_cols so negative numbers wrap around
+      left_cell = rem(n_c + shift_horiz, num_cols)
+
+      # ticks_to_simulate % col_len
+      shift_vert = rem(ticks_to_simulate, num_rows)
+      down_cell = rem(n_r - shift_vert + num_rows, num_rows)
+      # add num_rows so negative numbers wrap around
+      up_cell = rem(n_r + shift_vert, num_rows)
+
+      # NOTE: Important to filter to correct directional blizzard, e.g. ' == :right'
+      # so other directional blizzard at cell doesn't get counted.
+      # defaults of [] so that the bottom_right start pos (which is beyond row bound)
+      # doesn't cause nil problem
+      right_blizz? = (board.d_r_mat |> Enum.at(n_r, []) |> Enum.at(right_cell)) == :right
+      down_blizz? = (board.d_r_mat |> Enum.at(down_cell, []) |> Enum.at(n_c)) == :down
+
+      left_blizz? = (board.u_l_mat |> Enum.at(n_r, []) |> Enum.at(left_cell)) == :left
+      up_blizz? = (board.u_l_mat |> Enum.at(up_cell, []) |> Enum.at(n_c)) == :up
+
+      # check if a blizzard going in ANY direction will occupy {n_r, n_c}
+      # start pos is also always open if we haven't left it yet
+      [right_blizz?, down_blizz?, left_blizz?, up_blizz?] == [false, false, false, false]
+      or
+      ({n_r, n_c} == start and {r,c} == start)
+    end)
+  end
+end
+
+
+
+defmodule BlizzardBasin do
+
+
+  def traverse(board, start_dst, pos_steps, seen \\ MapSet.new)
 
   # abort when lower_bound on steps is >= min_steps
   # ~4x prune on example
-  def traverse(_, {_, _, {d_r, d_c}}, {{p_r, p_c}, steps_taken, min_steps}, seen)
+  def traverse(_, {_, {d_r, d_c}}, {{p_r, p_c}, steps_taken, min_steps}, seen)
   when (steps_taken + ((d_r-p_r) + (d_c-p_c))) >= min_steps do
     {min_steps, seen}
   end
 
   # arrived
-  def traverse(_, {_, _, dst}, {pos, steps_taken, _}, seen) when pos == dst do
-    IO.puts("ARRIVED #{steps_taken}")
-    {steps_taken, seen}
+  def traverse(_, {_, dst}, {pos, steps_taken, _}, seen) when pos == dst do
+    # dst is cell right before the actual destination, so add 1 to steps required,
+    # (destination cell is out-of-bounds and always empty)
+    {steps_taken+1, seen}
   end
 
-  # memoized blizzard movement, it's independent of our traversal
-  def traverse(blizz_lookup, {down_bound, right_bound, dst}, {pos, steps_taken, min_steps}, seen) do
+  def traverse(board, {start, dst}, {pos, steps_taken, min_steps}, seen) do
     # NOTE: can switch places with an adjacent blizzard in one turn
     # NOTE: can't share place with blizzard
     # NOTE: blizzards move first during turn
@@ -114,73 +149,37 @@ defmodule BlizzardBasin do
     # max, 5 possible actions
     # wait + 4 dirs to move
 
-    #IO.puts("#{steps_taken}")
-
-    # First, advance the blizzards
-    blizz_lookup = BlizzardBasin.advance_blizzards(blizz_lookup, steps_taken, {down_bound, right_bound})
-
-    # Second, get all valid possible moves, assumptions:
+    # get all valid possible moves, assumptions:
     #   1. Backtracking is allowed.
     #   2. Can swap position with blizzard on same turn.
     #   3. Cannot remain in-place if blizzard occupies current cell.
-
-    # So, check the advanced blizzards 'blizz_lookup', for up to max 5 moves into empty cells
-
-    blizz_state = Map.get(blizz_lookup, steps_taken)
-
-
-    {r, c} = pos
 
     # Order list by manhattan dist to destination
     # First group:  [down, right]
     # Second group: [remain]
     # Third group:  [up, left]
-    move_closer = [
-      # NOT going past edge AND NOT moving into blizzard
-      # down
-      (if (r+1) <= down_bound and blizz_state |> Map.get({r+1,c}) |> is_nil, do: {r+1,c}, else: nil),
-      # right
-      (if (c+1) <= right_bound and blizz_state |> Map.get({r,c+1}) |> is_nil, do: {r,c+1}, else: nil),
-    ]
+    valid_moves = Board.adj_empty_at_tick(board, pos, start, steps_taken + 1)
 
-    remain = [(if blizz_state |> Map.get({r,c}) |> is_nil, do: {r,c}, else: nil)]
-
-    move_farther = [
-      # NOT going past edge AND NOT moving into blizzard
-      # up
-      (if (r-1) >= 0 and blizz_state |> Map.get({r-1,c}) |> is_nil, do: {r-1,c}, else: nil),
-      # left
-      (if (c-1) >= 0 and blizz_state |> Map.get({r,c-1}) |> is_nil, do: {r,c-1}, else: nil),
-    ]
-
-    valid_moves = (move_closer ++ remain ++ move_farther) |>
-      List.flatten |>
-      Enum.filter(& &1 != nil)
-
-
-    # handle starting pos, don't allow moving left/right since row is -1
-    valid_moves = case pos do
-      # 2 possible moves from starting position
-      {-1, 0} -> Enum.filter(valid_moves, & &1 in [{0,0}, {-1,0}])
+    # if going bottom-left -> top-right,
+    # reverse valid_moves to make min l1 dist moves first
+    valid_moves = case dst do
+      {0,0} -> Enum.reverse(valid_moves)
       _ -> valid_moves
     end
 
 
-    #IO.inspect({steps_taken, pos}, label: "{steps_taken, pos}")
-    #IO.inspect({steps_taken, pos, valid_moves}, label: "{steps_taken, pos, valid_moves}")
-
     # Abort if path won't set new bound
     Enum.reduce_while(valid_moves, {min_steps, seen}, fn move, {min_steps_acc, seen_acc} ->
+      # can't improve by making more moves
       if (steps_taken+1) >= min_steps_acc do
         {:halt, {min_steps_acc, seen_acc}}
       else
-
         # if already traversed pos at given tick skip, else traverse
         case MapSet.member?(seen_acc, {elem(move,0), elem(move,1), steps_taken + 1}) do
           true -> {:cont, {min_steps_acc, seen_acc}}
           false ->
-            {n_min, n_seen} = BlizzardBasin.traverse(blizz_lookup,
-              {down_bound, right_bound, dst},
+            {n_min, n_seen} = BlizzardBasin.traverse(board,
+              {start, dst},
               {move, steps_taken + 1, min_steps_acc},
               MapSet.put(seen_acc, {elem(move,0), elem(move,1), steps_taken + 1})
             )
@@ -206,19 +205,32 @@ defmodule Main do
       String.trim_trailing |>
       String.split("\n")
 
-    blizz_lookup = BlizzardBasin.parse(lines)
+    board = Board.from_lines(lines)
 
-    # IO.inspect(blizz_lookup, label: "blizz_lookup")
+    # Set dsts to corner, add 1 for step to finish
+    top_left_start = {-1, 0}
+    down_right_start = {board.down_bound+1, board.right_bound}
 
-    start_pos = {-1, 0}
+    top_left_dst = {0,0}
+    down_right_dst = {board.down_bound, board.right_bound}
 
-    {down_bound, right_bound, dst} = BlizzardBasin.bounds_and_dst(lines)
-    # Set dst to bottom-right corner, add 1 for step to finish
+    results =
+    [
+      {top_left_start, down_right_dst},
+      {down_right_start, top_left_dst},
+      {top_left_start, down_right_dst}
+    ] |>
+    Enum.reduce([0],
+    fn {start, dst}, res_list ->
+      steps = BlizzardBasin.traverse(board, {start, dst}, {start, res_list |> hd(), :infinity}) |> elem(0)
+      [steps] ++ res_list
+    end) |>
+    Enum.reverse
 
-    {res_p1, _} = BlizzardBasin.traverse(%{ -1 => blizz_lookup }, {down_bound, right_bound, dst}, {start_pos, 0, :infinity}, MapSet.new([{-1,0,0}]))
+    [_,res_p1, _, res_p2] = results
 
-    IO.inspect(res_p1+1, label: "Part 1")
-
+    IO.inspect(res_p1, label: "Part 1")
+    IO.inspect(res_p2, label: "Part 2")
 
   end
 end
